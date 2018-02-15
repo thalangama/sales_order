aa8b92b53ae6c35497e2efb981f7680217273f17
thalangama <talangama@gmail.com>
2018-02-15 23:26:39 +0530

    db

--- dbManager/udaya.sql
+++ dbManager/udaya.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 15, 2018 at 12:24 PM
+-- Generation Time: Feb 15, 2018 at 06:54 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.0.21
 
@@ -43,7 +43,13 @@ CREATE TABLE `customer_details` (
 INSERT INTO `customer_details` (`id`, `nic`, `name`, `address`, `phone_no`) VALUES
 (1, '851202439v', 'sam HAR THA', 'medirigiriya', 774231616),
 (2, '851202440V', 'SHANI', '11 11', 123456792),
-(15, '', '', '', 0);
+(15, '', '', '', 0),
+(19, '1', '1', '1', 1),
+(20, '4554', '1111', '555', 222),
+(22, '45542', '1111', '555', 222),
+(23, '455422', '1111', '555', 222),
+(24, '4554222', '1111', '555', 222),
+(26, '4554222v', '1111', '555', 222);
 
 -- --------------------------------------------------------
 
@@ -64,8 +70,7 @@ CREATE TABLE `items` (
 INSERT INTO `items` (`id`, `code`, `description`) VALUES
 (1, '001', 'Samsung 32 tv'),
 (2, '002', 'Damro washine machine'),
-(3, '003', 'dell laptop'),
-(4, '004', 'lg ac ');
+(3, '003', 'dell laptop');
 
 -- --------------------------------------------------------
 
@@ -116,7 +121,9 @@ CREATE TABLE `orders` (
 
 INSERT INTO `orders` (`id`, `order_no`, `date`, `customer_id`, `sales_officer_id`, `recovery_officer_id`, `payment`, `payment_date`, `no_of_terms`) VALUES
 (6, '0004', '2018-02-11', 1, 1, 3, 100, '2018-02-12', 10),
-(7, '', '0000-00-00', 15, 0, 0, 0, '0000-00-00', 0);
+(7, '', '0000-00-00', 15, 0, 0, 0, '0000-00-00', 0),
+(8, '1', '0000-00-00', 19, 1, 1, 1, '0000-00-00', 1),
+(9, '222', '2018-02-19', 26, 1, 2, 1, '2018-02-11', 11);
 
 -- --------------------------------------------------------
 
@@ -138,7 +145,9 @@ CREATE TABLE `order_items` (
 --
 
 INSERT INTO `order_items` (`id`, `order_id`, `item_id`, `price`, `quantity`, `status`) VALUES
-(4, 6, 1, 1500, 2, 1);
+(4, 6, 1, 1500, 2, 1),
+(5, 8, 1, 11, 1, 1),
+(6, 9, 1, 11, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -164,7 +173,10 @@ INSERT INTO `payments` (`id`, `order_id`, `amount`, `payment_date`, `officer_id`
 (2, 7, 0, '0000-00-00', 0, 1),
 (3, 6, 1000, '2018-02-13', 3, 1),
 (4, 6, 200, '2018-02-28', 3, 1),
-(5, 6, 500, '2018-03-29', 3, 0);
+(5, 6, 500, '2018-03-29', 3, 0),
+(6, 8, 0, '0000-00-00', 1, 1),
+(7, 9, 0, '0000-00-00', 1, 1),
+(8, 0, 2, '2018-02-15', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -196,7 +208,19 @@ INSERT INTO `payment_plan` (`id`, `payment_date`, `payment_amount`, `term`, `ord
 (49, '2018-09-12', 290, 8, 6, 1),
 (50, '2018-10-12', 290, 9, 6, 1),
 (51, '2018-11-12', 290, 10, 6, 1),
-(53, '0000-00-00', 0, 1, 7, 1);
+(53, '0000-00-00', 0, 1, 7, 1),
+(54, '0000-00-00', 10, 1, 8, 1),
+(55, '2018-02-11', 0.90909090909091, 1, 9, 1),
+(56, '2018-03-11', 0.90909090909091, 2, 9, 1),
+(57, '2018-04-11', 0.90909090909091, 3, 9, 1),
+(58, '2018-05-11', 0.90909090909091, 4, 9, 1),
+(59, '2018-06-11', 0.90909090909091, 5, 9, 1),
+(60, '2018-07-11', 0.90909090909091, 6, 9, 1),
+(61, '2018-08-11', 0.90909090909091, 7, 9, 1),
+(62, '2018-09-11', 0.90909090909091, 8, 9, 1),
+(63, '2018-10-11', 0.90909090909091, 9, 9, 1),
+(64, '2018-11-11', 0.90909090909091, 10, 9, 1),
+(65, '2018-12-11', 0.90909090909091, 11, 9, 1);
 
 -- --------------------------------------------------------
 
@@ -220,7 +244,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `user_type`, `password`, `is_deleted`) VALUES
 (1, 'udaya', 'Udaya', 'Kumara', 'M', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2', 0),
-(2, 'sameera', 'sameera', 'harshana', 'O', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2', 0);
+(2, 'sameera', 'sameera', 'harshana', 'O', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 0);
 
 --
 -- Indexes for dumped tables
@@ -237,7 +261,8 @@ ALTER TABLE `customer_details`
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `code` (`code`);
 
 --
 -- Indexes for table `officer`
@@ -287,12 +312,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `customer_details`
 --
 ALTER TABLE `customer_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `officer`
 --
@@ -302,22 +327,22 @@ ALTER TABLE `officer`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `payment_plan`
 --
 ALTER TABLE `payment_plan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 --
 -- AUTO_INCREMENT for table `users`
 --
